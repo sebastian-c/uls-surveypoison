931b2c6ee62d9183221d9380597484f128c3dd92
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2022-11-28 16:41:10 +0100

    Added data to be read in

--- survey_analysis.R
+++ survey_analysis.R
@@ -6,8 +6,14 @@
 #library(limonade)
 library(tm) # https://www.r-bloggers.com/2021/05/sentiment-analysis-in-r-3/
 library(deeplr) # For translating French surveys
+library(readr)
+
 
 DEEPL_API_KEY = readLines("credentials/deepl_api_key.txt")
+
+raw_survey  = read_csv("raw_data/results-survey961781.csv")
+#raw_survey_fr  = read_csv("raw_data/results-survey961781_fr.csv")
+
 
 #### Text mining ####
 
